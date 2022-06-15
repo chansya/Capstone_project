624ba558302db7ca03cda65591fb51b8359d2959
Amy Chan <amy.chansya@gmail.com>
2022-06-15 14:27:28 -0700

    Create form for new record

--- server.py
+++ server.py
@@ -26,8 +26,8 @@ def login():
     """View login page."""
     return render_template('login.html')
 
-@app.route("/verify_login",methods=["POST"])
-def verify_login():
+@app.route("/process_login",methods=["POST"])
+def process_login():
     """Verify user's login credentials."""
 
     email = request.form.get("email")
@@ -40,7 +40,7 @@ def verify_login():
     else:
         # Log in user by storing the user's email in session
         session["user_email"] = user.email
-        flash(f"Welcome back, {user.name}!")
+        flash(f"Welcome, {user.name}!")
         return redirect("/progress")
         
 @app.route("/signup")
@@ -54,63 +54,90 @@ def create_account():
     name = request.form.get("name")
     email = request.form.get("email")
     password = request.form.get("password")
-    confirm_pw = request.form.get("confirm_pw")
     
     user = User.get_by_email(email)
     if user:
         flash("Email already exists. Please log in.")
-        return redirect("/signup")
+        return redirect("/login")
     else:
         user = User.create(name, email, password)
         db.session.add(user)
         db.session.commit()
-        flash("Account created! Please log in.")
-        return redirect("/login")
+        session["user_email"] = user.email
+        flash(f"Account created. Welcome, {user.name}!")
+        return redirect("/progress")
 
 @app.route("/progress")
 def view_progress():
     """View the progress page."""
     user = User.get_by_email(session["user_email"])
-    habits = Habit.get_by_user(user.user_id)
-    return render_template("progress.html", user=user, habits=habits)
-
-
-@app.route("/new_habit")
-def new_habit():
-    return render_template("new_habit.html")
+    if user:
+        habits = Habit.get_by_user(user.user_id)
+        return render_template("progress.html", user=user, habits=habits)
+    else: 
+        return redirect('/')
 
 
 @app.route("/create_habit", methods = ["POST"])
 def create_habit():
     """Create new habit object and add into database."""
     
-    # extract the input from the request
+    # extract the user input from the request
     habit_name = request.json.get("habit_name")
     frequency = request.json.get("frequency")
     time_period = request.json.get("time_period")
     start_date  = datetime.strptime(
                      request.json.get("start_date"),
                      '%Y-%m-%d')
-    
     current_streak = 0
     max_streak = 0
     
     user = User.get_by_email(session.get("user_email"))
-    # create new habit object
+    # create new habit object in database
     habit = Habit.create(user.user_id, habit_name,frequency,time_period,current_streak,max_streak, start_date)
     db.session.add(habit)
     db.session.commit()
     flash("Habit created!")
    
-    habits = Habit.get_by_user(user.user_id)
+    # put information to send back as response in dictionary to jsonify
+    habit_to_send = {"habit_name": habit.habit_name,
+         "current_streak": habit.current_streak,
+         "max_streak": habit.max_streak,
+         "frequency": habit.frequency,
+         "time_period": habit.time_period,
+         "start_date": habit.start_date,
+        }
+    return jsonify(habit_to_send)
+
+
+@app.route("/create_record", methods=["POST"])
+def create_record():
+    """Create new record object and add into database."""
+
+    # extract the input from the request
+    habit_id = request.json.get("habit_id")
+    finished = request.json.get("finished")
+    notes = request.json.get("notes")
+    record_date  = datetime.strptime(
+                     request.json.get("record_date"),
+                     '%Y-%m-%d')
+
+    # create new record object
+    record = Record.create(habit_id, finished, notes, record_date)
+    db.session.add(record)
+    db.session.commit()
+    flash("Record saved!")
+
+    # add current streak by 1 and update max streak if needed 
 
     return {"success": True,
-            "status": f"{habit.habit_name} has been added"}
+            "status": f"Record has been added."}
 
-
-# @app.route("/create_record")
-# def create_record():
-#     return render_template("/progress")
+@app.route("/logout")
+def process_logout():
+    del session["user_email"]
+    flash("Logged out.")
+    return redirect("/")
 
 
 if __name__ == "__main__":
--- static/js/habit_form.js
+++ static/js/habit_form.js
@@ -2,15 +2,18 @@ const newHabitBtn = document.querySelector('#show_form');
 const habitForm = document.querySelector('#habit_form');
 const cancelBtn = document.querySelector('#cancel_form');
 const saveBtn = document.querySelector('#save_form');
+
 // display the new habit form when user clicks on "Add a habit" btn
 newHabitBtn.addEventListener('click', ()=>{
     habitForm.style.display = 'block';
 })
-// hide the form when user clicks cancel
+
+// hide the form when user clicks "Cancel" btn
 cancelBtn.addEventListener('click', ()=>{
     habitForm.style.display = 'none';
 })
 
+// send AJAX post request when user submits the new habit form
 habitForm.addEventListener('submit',(evt)=>{
     evt.preventDefault();
 
@@ -29,13 +32,58 @@ habitForm.addEventListener('submit',(evt)=>{
         },
     })
         .then(response => response.json())
-        .then(responseJson=>{
-            alert(responseJson.status);
+        .then(habitData=>{
+            let prompt = document.querySelector('#new_habit_prompt');
+            if(prompt){prompt.remove();}
+           
             document.querySelector('#habit_list').insertAdjacentHTML('beforeend', 
-            `<p>${formInput.habit_name}</p>
-            <p>Current streak: 0</p>
-            <p>Longest streak: 0</p>
-            <span>${formInput.frequency} ${formInput.time_period}</span>
-            <button>Done!</button>`);
+            `<p>${habitData.habit_name} (${habitData.frequency} times ${habitData.time_period})</p>
+            <p>Current streak: ${habitData.current_streak}</p>
+            <p>Longest streak: ${habitData.max_streak}</p>
+            <hr>`);
+
+            document.querySelector('#habit_done').insertAdjacentHTML('beforeend',
+            `<option value="${habitData.habit_name}">${habitData.habit_name}</option>`
+            )
         })
 })
+
+
+// select the list forms
+// const recordForms = document.querySelectorAll('.record_form');
+
+// for(const recordForm of recordForms){
+//     recordForm.addEventListener('submit',(evt)=>{
+//         const form = evt.target;
+//         evt.preventDefault();
+
+//         const formInput = {
+//             habit_id : form.id,
+//             finished : document.querySelector('#finished').value,
+//             notes : document.querySelector('#notes').value,
+//             record_date : document.querySelector('#record_date').value
+//         }
+    
+//         fetch('/create_record', {
+//             method: 'POST',
+//             body: JSON.stringify(formInput),
+//             headers:{
+//                 'Content-Type' : 'application/json',
+//             },
+//         })
+//             .then(response => response.json())
+//             .then(responseJson=>{
+//                 alert(responseJson.status);
+//             })
+//     })
+// }
+
+
+// for rendering calendar
+document.addEventListener('DOMContentLoaded', function() {
+    let calendarEl = document.getElementById('calendar');
+    let calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth'
+    });
+    calendar.render();
+  });
