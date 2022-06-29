e312c2d747557ef791493937b1df9b4878f08b79
Amy Chan <amy.chansya@gmail.com>
2022-06-29 13:40:07 -0700

    Revise badges logic to list potential badges with exisiting ones

--- server.py
+++ server.py
@@ -95,7 +95,25 @@ def signup():
             
             # Create badge 1 for sign up
             badge1 = Badge.create(user.user_id, "static/img/1.png", "Our New Star", "Register an account")
+            badge2 = Badge.create(user.user_id, "static/img/2bw.png", "First Step", "Create first habit")
+            badge3 = Badge.create(user.user_id, "static/img/3bw.png", "From 0 To 1", "Create first record")
+            badge4 = Badge.create(user.user_id, "static/img/4bw.png", "Multi-tasker", "Create 3 habits")
+            badge5 = Badge.create(user.user_id, "static/img/5bw.png", "5-Star Records", "Create 5 records")
+            badge6 = Badge.create(user.user_id, "static/img/6bw.png", "Perfect 10", "Create 10 records")
+            badge7 = Badge.create(user.user_id, "static/img/7bw.png", "Up We go", "Reach 7 streaks")
+            badge8 = Badge.create(user.user_id, "static/img/8bw.png", "Unstoppable", "Reach 30 streaks")
+            badge9 = Badge.create(user.user_id, "static/img/9bw.png", "Streak Master", "Reach 100 streak")
+            
+            
             db.session.add(badge1)
+            db.session.add(badge2)
+            db.session.add(badge3)
+            db.session.add(badge4)
+            db.session.add(badge5)
+            db.session.add(badge6)
+            db.session.add(badge7)
+            db.session.add(badge8)
+            db.session.add(badge9)
             db.session.commit()
             flash("Yay! You've earned a badge for creating an account! Check it under your profile.")
             return redirect("/progress")
@@ -111,8 +129,9 @@ def view_progress():
         
         habits = user.habits
         # habits = Habit.query.filter(Habit.user_id==user.user_id).order_by(Habit.habit_id.asc()).all()
-        badges = user.badges
         
+        badges = Badge.query.filter(Badge.user_id==user.user_id).order_by(Badge.img_url).all()
+   
         # loop over habit list to populate event list for calendar
         events = []
         habit_colors = ['#c5dedd','#bcd4e6','#fad2e1','#eddcd2','#cddafd',
@@ -172,22 +191,20 @@ def create_habit():
 
     # Check for any existing badge 2 to avoid duplicate
     badge2 = Badge.query.filter(Badge.user_id==user.user_id,
-                                 Badge.img_url=="static/img/2.png").first()
-    if Habit.count_habit_by_user(user.user_id) == 1 and badge2 == None:
+                                 Badge.img_url=="static/img/2bw.png").first()
+    if Habit.count_habit_by_user(user.user_id) == 1 and badge2 :
         # Create badge 2 for first habit
-        badge2 = Badge.create(user.user_id, "static/img/2.png", "First Step", "Create first habit")
-        db.session.add(badge2)
+        badge2.img_url = "static/img/2.png"
         db.session.commit()
         flash("Awesome! You've created your first habit and earned a badge!")
 
     # Check for any existing badge 4 to avoid duplicate
     badge4 = Badge.query.filter(Badge.user_id==user.user_id,
-                                 Badge.img_url=="static/img/4.png").first()
+                                 Badge.img_url=="static/img/4bw.png").first()
 
-    if Habit.count_habit_by_user(user.user_id) == 3 and badge4 == None:
+    if Habit.count_habit_by_user(user.user_id) == 3 and badge4:
         # create badge 4 for third habits
-        badge4 = Badge.create(user.user_id, "static/img/4.png", "Multi-tasker", "Create 3 habits")
-        db.session.add(badge4)
+        badge4.img_url = "static/img/4.png"
         db.session.commit()
         flash("Wow! You've created your three habits and earned a badge!")
 
@@ -231,40 +248,60 @@ def create_record():
     db.session.add(record)
     db.session.commit()
     
+
     # Create badges according to number of records
     user = User.get_by_email(session["user_email"])
     habits = Habit.get_by_user(user.user_id)
     record_count = 0
+    badge7 = Badge.query.filter(Badge.user_id==user.user_id,
+                                 Badge.img_url=="static/img/7bw.png").first()
+    badge8 = Badge.query.filter(Badge.user_id==user.user_id,
+                                 Badge.img_url=="static/img/8bw.png").first()
+    badge9 = Badge.query.filter(Badge.user_id==user.user_id,
+                                 Badge.img_url=="static/img/9bw.png").first()
     for habit in habits:
         record_count +=  Record.count_records_by_habit(habit.habit_id)
+        Habit.update_curr_streak(habit.habit_id)
+        Habit.update_max_streak(habit.habit_id)
+        if habit.max_streak == 7 and badge7:
+            badge7.img_url = "static/img/7.png"
+            db.session.commit()
+            flash("Wonderful! You've reached 7 streaks and earned a badge!")
+        elif habit.max_streak == 30 and badge8:
+            badge7.img_url = "static/img/8.png"
+            db.session.commit()
+            flash("Dope! You've reached 30 streaks and earned a badge!")
+
+        elif habit.max_streak == 100 and badge9:
+            badge7.img_url = "static/img/9.png"
+            db.session.commit()
+            flash("Is that real!? You've reached 100 streaks and earned a badge!")
+
+        
     
     # Check for any existing badges to avoid duplicate
     badge3 = Badge.query.filter(Badge.user_id==user.user_id,
-                                 Badge.img_url=="static/img/3.png").first()
+                                 Badge.img_url=="static/img/3bw.png").first()
     badge5 = Badge.query.filter(Badge.user_id==user.user_id,
-                                 Badge.img_url=="static/img/5.png").first()
+                                 Badge.img_url=="static/img/5bw.png").first()
     badge6 = Badge.query.filter(Badge.user_id==user.user_id,
-                                 Badge.img_url=="static/img/6.png").first()
+                                 Badge.img_url=="static/img/6bw.png").first()
                                 
 
-    if record_count == 1 and badge3 == None:
-        badge3 = Badge.create(user.user_id, "static/img/3.png", "From 0 To 1", "Create first record")
-        db.session.add(badge3)
+    if record_count == 1 and badge3:
+        badge3.img_url = "static/img/3.png"
         db.session.commit()
-        flash("You've created your first record and earned a badge!")
+        flash("Great! You've created your first record and earned a badge!")
 
-    if record_count == 5 and badge5 == None:
-        badge5 = Badge.create(user.user_id, "static/img/5.png", "5-Star Records", "Create 5 records")
-        db.session.add(badge5)
+    if record_count == 5 and badge5:
+        badge5.img_url = "static/img/5.png"
         db.session.commit()
-        flash("You've created your 5 records and earned a badge!")
+        flash("Amazing! You've created your 5 records and earned a badge!")
     
-    if record_count == 10 and badge6 == None:
-        badge6 = Badge.create(user.user_id, "static/img/6.png", "Perfect 10", "Create 10 records")
-        db.session.add(badge6)
+    if record_count == 10 and badge6:
+        badge6.img_url = "static/img/6.png"
         db.session.commit()
-        flash("You've created your 10 records and earned a badge!")
-
+        flash("Spectacular! You've created your 10 records and earned a badge!")
 
     return redirect("/progress")
 
@@ -275,7 +312,8 @@ def view_habits():
     user = User.get_by_email(session.get("user_email"))
     
     habits = user.habits
-    badges = user.badges
+    badges = Badge.query.filter(Badge.user_id==user.user_id).order_by(Badge.img_url).all()
+
    
     return render_template("all_habits.html", user=user, habits=habits,badges=badges)
 
@@ -284,8 +322,8 @@ def react_view_habits():
     """View all habits."""
     user = User.get_by_email(session["user_email"])
     habits = Habit.get_by_user(user.user_id)
-    badges = Badge.get_by_user(user.user_id)
-    print(badges)
+    badges = Badge.query.filter(Badge.user_id==user.user_id).order_by(Badge.img_url).all()
+
     return render_template("habits.html", user=user, habits=habits, badges=badges)
 
 
